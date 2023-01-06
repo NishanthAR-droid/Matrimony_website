2ae438a8205885411259c2282e77c54650f059ee
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-01-06 22:28:20 +0530

    1

--- PHP/logout.php
+++ PHP/logout.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
     include("db_connect.php");
     session_start();
     if (isset($_SESSION['username'])) {
@@ -12,4 +13,13 @@
       {
         header("location:home.php");
       }
+=======
+include("db_connect.php");
+session_start();
+session_unset();
+session_destroy();
+echo "<script>window.location.href='home.php';
+                  alert('You have logged out successfully');
+                  </script>";
+>>>>>>> 2e6872ec3c935cde74b7dcde0401362ef08f607a
 ?>
