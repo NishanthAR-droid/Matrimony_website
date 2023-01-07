30313468a7a0c12c980395ee7001c801f4a6cf8b
Prajwal-Surendra <prajwalsurendra7178@gmail.com>
2023-01-07 16:57:54 +0530

    Feedback

--- PHP/uploadFeedback.php
+++ PHP/uploadFeedback.php
@@ -3,27 +3,24 @@ include("db_connect.php");
 session_start();
 if (isset($_POST["submit"])) {
     $username = $_SESSION['username'];
-    $rating=$_POST[""];
-    $feedback=$_POST[""];
+    $rating = $_POST[""];
+    $feedback = $_POST[""];
 
-    $sql="SELECT *FROM `user_feedback` WHERE `uname`='$username'";
-    $result=mysqli_query($conn,$sql);
-    $count=mysqli_num_rows($result);
-    if($count==1)
-    {
+    $sql = "SELECT *FROM `user_feedback` WHERE `uname`='$username'";
+    $result = mysqli_query($conn, $sql);
+    $count = mysqli_num_rows($result);
+    if ($count == 1) {
         echo "<script>window.location.href='userhome.php'; 
         alert('You have already given feedback');
-        </script>"; 
-    }
-    else
-    {
-        $sql1="INSERT INTO `user_feedback` (`uname`,`rating`,`feedback`) VALUES ('$username','$rating','$feedback')";
-        $result1=mysqli_query($conn,$sql1);
-        if($result1)
-        {
+        </script>";
+    } else {
+        $sql1 = "INSERT INTO `user_feedback` (`uname`,`rating`,`feedback`) VALUES ('$username','$rating','$feedback')";
+        $result1 = mysqli_query($conn, $sql1);
+        if ($result1) {
             echo "<script>window.location.href='userhome.php'; 
             alert('Thank you for your feedback');
-            </script>"; 
+            </script>";
         }
     }
+}
 ?>
