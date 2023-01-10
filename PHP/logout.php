51b79d66ded5c435026c45498e2de6e9b80a2ac6
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-01-10 16:49:01 +0530

    Update logout.php

--- PHP/logout.php
+++ PHP/logout.php
@@ -1,6 +1,9 @@
 <?php
 include("db_connect.php");
 session_start();
+if (!isset($_SESSION['username'])) {
+    header("location:home.php");
+  } 
 session_unset();
 session_destroy();
 echo "<script>window.location.href='home.php';
